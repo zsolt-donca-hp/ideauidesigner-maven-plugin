7149da0fd773a2972dcabde47d2291e9e236ab37
Zsolt Donca <zsolt.donca@hp.com>
2015-06-24 18:27:42 +0300

    Upgrading to IDEA version 14.1.3, and using the jdom.jar artifact from within the IntelliJ installation directory to avoid the following linkage error:
    NoSuchMethodError: org.jdom.Element.clone()Lorg.jdom.Element;
    
    that is, a synthetic method typically generated by the compiler when overriding a method with covariant return type. This covariant override doesn't seem to be present in any of the released jdom versions (I wonder what jdom version is IDEA using).

--- install-intellij-libs.sh
+++ install-intellij-libs.sh
@@ -4,7 +4,7 @@ INTELLIJ_HOME=$1
 
 if [ -z "$INTELLIJ_HOME" ]
 then
-  echo "Please provide the path to Intellij home directory. For example: install-intellij-libs.sh /Applications/Nika-IU-111.228.app"
+  echo "Please provide the path to IntelliJ home directory (14.1.3). For example: install-intellij-libs.sh \"C:\Program Files (x86)\JetBrains\IntelliJ IDEA 14.1.3\""
   exit 1
 fi
 
@@ -17,6 +17,9 @@ fi
 echo 'Installing Intellij artifacts to Maven local repository'
 echo "Intellij home: $INTELLIJ_HOME"
 
-mvn install:install-file -Dfile="$INTELLIJ_HOME/lib/javac2.jar" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0.3 -Dpackaging=jar
-mvn install:install-file -Dfile="$INTELLIJ_HOME/lib/asm-all.jar" -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0.3 -Dpackaging=jar
-mvn install:install-file -Dfile="$INTELLIJ_HOME/lib/forms_rt.jar" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0.3 -Dpackaging=jar
+mvn install:install-file -Dfile="$INTELLIJ_HOME/redist/javac2.jar" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.1.3 -Dpackaging=jar
+mvn install:install-file -Dfile="$INTELLIJ_HOME/redist/src/src_javac2.zip" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.1.3 -Dpackaging=jar -Dclassifier=sources
+mvn install:install-file -Dfile="$INTELLIJ_HOME/redist/forms_rt.jar" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.1.3 -Dpackaging=jar
+mvn install:install-file -Dfile="$INTELLIJ_HOME/redist/src/src_forms_rt.zip" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.1.3 -Dpackaging=jar -Dclassifier=sources
+mvn install:install-file -Dfile="$INTELLIJ_HOME/lib/asm-all.jar" -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.1.3 -Dpackaging=jar
+mvn install:install-file -Dfile="$INTELLIJ_HOME/lib/jdom.jar" -DgroupId=com.intellij -DartifactId=jdom -Dversion=14.1.3 -Dpackaging=jar
